5bba54b7bb9bdf87bb6c7838c6e7078352cea646
prithvikr66 <pkunofficial66@gmail.com>
2024-07-11 01:21:27 +0530

    footer modifications done

--- src/components/Alpha.tsx
+++ src/components/Alpha.tsx
@@ -1,44 +1,48 @@
 import { useRecoilValue } from "recoil";
 import JeaIcon from "../assets/JeaIcon.svg";
 import { websiteThemeState } from "../atoms/website-theme";
-import { CopyTextIcon } from "./Icons";
+// import { CopyTextIcon } from "./Icons";
 
 const Alpha = () => {
   const websiteTheme = useRecoilValue(websiteThemeState);
   return (
-    <div className="h-[75%] w-full">
+    <div
+      className="h-[75%] w-full flex flex-col "
+    >
       <div
         style={{ color: websiteTheme.textColor }}
-        className="flex w-[90%] mx-auto"
+        className="flex w-[90%] mx-auto mt-[50px] "
       >
         <div>
-          <img src={JeaIcon} alt="Jea Icon" />
+          <img src={JeaIcon} />
         </div>
-        <div className="flex flex-col gap-[5px]  w-[90%] mx-auto">
-          <p className="uppercase text-[14px]">Jeai</p>
-          <p className="uppercase text-[16px] break-words">
-            FV56CmR7fhEyPkymKfmviKV48uPo51ti9kAxssQqTDLu
-            <span>
+        <div className="flex flex-col gap-[5px]  w-[90%] pl-[10px]">
+          <p className="uppercase text-[14px]">alpha_hoe</p>
+          <div className=" ">
+            <p className=" uppercase text-[16px] break-words">FV56CmR7fhEyPkymKfmviKV48uPo51ti9kAxssQqTDLu</p>
+            {/* <span>
               <CopyTextIcon />
-            </span>
-          </p>
-          <p className="italic text-[16px] text-center">
+            </span> */}
+          </div>
+          <p className="italic text-[16px] mt-[10px]">
             “aped my life savings in Walter, this is FA”
           </p>
 
-          <div
-            className="h-[1px] w-[90%] mx-auto"
+         
+        </div>
+
+      </div>
+      <div
+            className="h-[1px] w-[90%] mx-auto mt-[20px]"
             style={{
               background: `linear-gradient(to right, ${websiteTheme.bgColor}, ${websiteTheme.textColor}, ${websiteTheme.bgColor})`,
             }}
           />
-        </div>
-      </div>
       <div
         style={{ color: websiteTheme.textColor }}
         className="flex flex-col gap-[10px]"
       >
-        <p className="text-center text-[20px] uppercase">
+        <p className="text-center text-[20px] uppercase mt-[100px]">
           highest quality alpha from selected callers
         </p>
         <p className="text-center text-[20px] uppercase">v v soon</p>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,6 @@
+import { useRecoilValue } from "recoil";
+import { websiteThemeState } from "../atoms/website-theme";
+
 const Footer = ({
   chatState,
   setChatState,
@@ -5,45 +8,44 @@ const Footer = ({
   chatState: any;
   setChatState: any;
 }) => {
+  const websiteTheme = useRecoilValue(websiteThemeState);
   return (
     <div className="lg:hidden h-[8%] w-[75%] mx-auto   ">
-      <div className=" h-[32px] w-full bg-white rounded-[4px] flex justify-between items-center p-[2px] pl-[2px] pr-[2px]">
+      <div
+        className={`h-[32px] w-full bg-white rounded-[4px] flex justify-between items-center p-[2px] pl-[2px] pr-[2px] ${
+          websiteTheme.bgColor === "#ffffff" && "border border-black"
+        }`}
+      >
         <div
-        onClick={()=>setChatState("DEN")}
-          className={` uppercase text-[14px]  h-[30px] w-[50px] flex items-center justify-center rounded-[2px]  ${
-            chatState === "DEN"
-              ? "bg-[#0000FF] text-white"
-              : "bg-none text-[#3d3d3d]"
-          }`}
-          //   style={{
-          //     color: "#3D3D3D",
-          //   }}
+          style={{
+            backgroundColor:
+              chatState === "DEN" ? websiteTheme.bgColor : "#ffffff",
+            color: chatState === "DEN" ? websiteTheme.textColor : "#000000",
+          }}
+          onClick={() => setChatState("DEN")}
+          className={` uppercase text-[14px]  h-[30px] w-[90px] flex items-center justify-center rounded-[2px]`}
         >
           den
         </div>
         <div
-        onClick={()=>setChatState("PUMP")}
-          className={` uppercase text-[14px] ml-[25px] h-[30px] w-[50px] flex items-center justify-center rounded-[2px]  ${
-            chatState === "PUMP"
-              ? "bg-[#0000FF] text-white"
-              : "bg-none text-[#3d3d3d]"
-          }`}
-          //   style={{
-          //     color: "#3D3D3D",
-          //   }}
+          style={{
+            backgroundColor:
+              chatState === "PUMP" ? websiteTheme.bgColor : "#ffffff",
+            color: chatState === "PUMP" ? websiteTheme.textColor : "#000000",
+          }}
+          onClick={() => setChatState("PUMP")}
+          className={` uppercase text-[14px] ml-[25px] h-[30px] w-[90px] flex items-center justify-center rounded-[2px]  `}
         >
           PUMP
         </div>
         <div
-        onClick={()=>setChatState("ALPHA")}
-          className={` uppercase text-[14px] ml-[25px] h-[30px] w-[50px] flex items-center justify-center rounded-[2px]  ${
-            chatState === "ALPHA"
-              ? "bg-[#0000FF] text-white"
-              : "bg-none text-[#3d3d3d]"
-          }`}
-          //   style={{
-          //     color: "#3D3D3D",
-          //   }}
+          style={{
+            backgroundColor:
+              chatState === "ALPHA" ? websiteTheme.bgColor : "#ffffff",
+            color: chatState === "ALPHA" ? websiteTheme.textColor : "#000000",
+          }}
+          onClick={() => setChatState("ALPHA")}
+          className={` uppercase text-[14px] ml-[25px] h-[30px] w-[90px] flex items-center justify-center rounded-[2px]`}
         >
           alpha
         </div>
